8abce4059eb9346dca3d8d34bac20389394d40a2
markshevchenko <markshevchenko@gmail.com>
2015-10-31 15:37:51 -0700

    Techdebt #6: Empty unit-tests BasicScannerExpressionExtensionsTests is created.

---
+++ LearningBasic.Test/Parsing/BasicScannerExpressionExtensionsTests.cs
@@ -0,0 +1,13 @@
+﻿namespace LearningBasic.Test.Parsing
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BasicScannerExpressionExtensionsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
