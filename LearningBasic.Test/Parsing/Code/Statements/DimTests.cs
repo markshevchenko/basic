27150e9c80d53e58e516269b037624a4d41dd529
markshevchenko <markshevchenko@gmail.com>
2015-11-11 22:42:18 +0300

    Issue #17: 3 new statement unit-test implemented.

---
+++ LearningBasic.Test/Parsing/Code/Statements/DimTests.cs
@@ -0,0 +1,51 @@
+﻿namespace LearningBasic.Parsing.Code.Statements
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using LearningBasic.Parsing.Code.Expressions;
+
+    [TestClass]
+    public class DimTests : BaseTests
+    {
+        [TestMethod]
+        public void Execute_OfDim_CreatesArray()
+        {
+            var indexes = new[] { new Constant(100), new Constant(200) };
+            var array = new ArrayVariable("A", indexes);
+            var dim = new Dim(array);
+            var rte = MakeRunTimeEnvironment();
+            dim.Execute(rte);
+
+            var actual = rte.Variables["A"];
+
+            Assert.IsInstanceOfType(actual, typeof(object[,]));
+        }
+
+        [TestMethod]
+        public void Execute_OfDim_SetsWidht()
+        {
+            var indexes = new[] { new Constant(100), new Constant(200) };
+            var array = new ArrayVariable("A", indexes);
+            var dim = new Dim(array);
+            var rte = MakeRunTimeEnvironment();
+            dim.Execute(rte);
+
+            var actual = (object[,])rte.Variables["A"];
+
+            Assert.AreEqual(99, actual.GetUpperBound(0));
+        }
+
+        [TestMethod]
+        public void Execute_OfDim_SetsHeight()
+        {
+            var indexes = new[] { new Constant(100), new Constant(200) };
+            var array = new ArrayVariable("A", indexes);
+            var dim = new Dim(array);
+            var rte = MakeRunTimeEnvironment();
+            dim.Execute(rte);
+
+            var actual = (object[,])rte.Variables["A"];
+
+            Assert.AreEqual(199, actual.GetUpperBound(1));
+        }
+    }
+}
---
+++ LearningBasic.Test/Parsing/Code/Statements/ForNextTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LearningBasic.Parsing.Code.Expressions;
+
+namespace LearningBasic.Parsing.Code.Statements
+{
+    [TestClass]
+    public class ForNextTests : BaseTests
+    {
+        [TestMethod]
+        public void Execute_OfForNext_RepeatsInnerStatement()
+        {
+            var rte = MakeRunTimeEnvironment();
+            var i = new ScalarVariable("I");
+            var from = new Constant(1);
+            var to = new Constant(10);
+            var step = new Constant(1);
+            var shoudBeEqualTo50 = 0;
+            var forNext = new ForNext(i, from, to, step, MakeStatement(() => { shoudBeEqualTo50 += 5; }));
+
+            forNext.Execute(rte);
+
+            Assert.AreEqual(50, shoudBeEqualTo50);
+        }
+    }
+}
